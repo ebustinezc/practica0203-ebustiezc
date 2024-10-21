8277b657bbdc4d8f8e61b98710e79cb5bdc4272a
ebustinezc <ebustinezc@educacion.navarra.es>
2024-10-21 11:56:35 +0200

    Ejercicio3

---
+++ Ejercicio3.py
@@ -0,0 +1,6 @@
+# Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+n = int(input('Dime un numero entero:'))
+if n % 2 == 0:
+    print('Es un numero par')
+else:
+    print('Es un numero impar')
