cbc72cac80f8f1e79ab44c8a25d7cdf384c79222
ebustinezc <ebustinezc@educacion.navarra.es>
2024-10-21 11:35:36 +0200

    Ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,8 @@
+# Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña e imprima por
+# pantalla si la contraseña introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
+contraseña = ('contraseña')
+contraseña_de_usuario = input('escribe tu contraseña:')
+if contraseña ==contraseña_de_usuario.lower():
+    print('su contraseña es correcta')
+else:
+    print('su contraseña es correcta')
