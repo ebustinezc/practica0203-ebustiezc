bedf4ecfdbbd5ceac1dff868cddffa6d04fc429c
ebustinezc <ebustinezc@educacion.navarra.es>
2024-10-25 09:07:23 +0200

    Ejercicio 8

---
+++ Ejercicio8.py
@@ -0,0 +1,6 @@
+# Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
+for n in range(1,11):
+    for  m in range(1,11):
+        print (m * n)
+    print('\n')
+input()
