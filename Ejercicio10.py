03c6a020875ec1d3c5344c16eb271fe1728285f4
ebustinezc <ebustinezc@educacion.navarra.es>
2024-10-25 09:21:56 +0200

    Ejercicio 10

---
+++ Ejercicio10.py
@@ -0,0 +1,9 @@
+# Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña 
+# hasta que introduzca la contraseña correcta.
+n = input('Dime tu contraseña:')
+m = input('Escribe tu contraseña:')
+while n != m:
+    print('CONTRASEÑA INCORRECTA')
+    m = input('Vuelve a introducir la contraseña:')
+print('CONTRASEÑA CORRECTA:)')
+input()
